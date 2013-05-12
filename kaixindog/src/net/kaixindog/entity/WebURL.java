80c5cf1e6aba6b79c8bfd64f5106aa16841d5f8c
lixiaoming502 <lixiaoming0502@gmail.com>
2013-05-12 16:26:58 +0800

    a

--- kaixindog/src/net/kaixindog/entity/WebURL.java
+++ kaixindog/src/net/kaixindog/entity/WebURL.java
@@ -1,5 +1,15 @@
 package net.kaixindog.entity;
 
 public class WebURL {
+	
+	private String url;
 
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
 }
